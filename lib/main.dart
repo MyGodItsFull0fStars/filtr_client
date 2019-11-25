9db5e685b0593d1982161ec6c9358b509e3f1f72
leusl <dleustik@hotmail.com>
2019-11-25 16:13:04 +0100

    Init branch

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Filtr'),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                      color: Colors.amber,
+                      
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      height: 20,
+                      width: double.infinity,
+                      color: Colors.black26,
+                      child: Text(
+                        "FILTER",
+                        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+                        ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                    children: <Widget>[
+                      FilterCard(),
+                      FilterCard(),
+                      FilterCard(),
+                      FilterCard(),
+                      FilterCard(),
+                      FilterCard(),
+                      FilterCard(),
+                      FilterCard(),
+                      FilterCard(),                    
+                    ],
+                  )
+                )
+              )
+                  ],
+                )
+              ),
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
+
+class FilterCard extends StatelessWidget{
+
+Widget build(BuildContext context){
+  return Card(
+    child: GestureDetector(
+      child: Container(
+      child: Text("Card 3"),
+        height: 150,
+        width: 150,
+      ),
+      onTap: () => {
+Scaffold.of(context).showSnackBar(SnackBar(
+      content: Text("Card touched"),
+    ))
+      },
+    )
+  );
+  }
+}
---
+++ lib/start.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Start());
+
+class Start extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(
+          appBar: AppBar(
+
+          ),
+          body: Container(
+            child: Column(
+             children: <Widget>[
+               
+             ],
+           ),
+          ),
+      );
+  }
+}
